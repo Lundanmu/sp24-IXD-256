0b93259f7a0c3663713562cf82c371aec8f9135a
Nikita Pashenkov <nikitapashenkov@gmail.com>
2024-02-09 12:18:03 -0800

    Update adc_to_rgb.py

--- class04/adc_to_rgb.py
+++ class04/adc_to_rgb.py
@@ -13,15 +13,15 @@ adc1_val = None
 rgb = None
 
 def setup():
-  global label0, adc1
+  global label0, adc1, rgb
   M5.begin()
   # initialize dispaly title and label:
   title0 = Widgets.Title("ADC test", 3, 0x000000, 0xffffff, Widgets.FONTS.DejaVu18)
   label0 = Widgets.Label("--", 3, 20, 1.0, 0xffffff, 0x000000, Widgets.FONTS.DejaVu18)
   # initialize analog to digital converter on pin 1:
   adc1 = ADC(Pin(1), atten=ADC.ATTN_11DB)
-  # initialize RGB LED strip on pin 38 with 10 pixels:
-  rgb = RGB(io=38, n=10, type="SK6812")
+  # initialize RGB LED strip on pin 38 with 30 pixels:
+  rgb = RGB(io=38, n=30, type="SK6812")
   # fill RGB LEDs with green:
   #rgb.fill_color(0x003300)
   # fill RGB LEDs with orange:
@@ -41,15 +41,21 @@ def map_value(in_val, in_min, in_max, out_min, out_max):
   return int(out_val)
 
 def loop():
-  global label0, adc1, adc1_value
+  global label0, adc1, adc1_value, rgb
   M5.update()
-  # read ADC value:
+  # read 12-bit ADC value (0 - 4095 range):
   adc1_val = adc1.read()
+  # convert ADC value to 8 bits (0 - 255 range):
+  adc1_val_8bit = map_value(adc1_val, in_min=0, in_max=4095, out_min=0, out_max=255)
   # print the ADC value:
-  print(adc1_val)
+  #print(adc1_val)
+  print(adc1_val_8bit)
   # show ADC value on display label:
   label0.setText(str(adc1_val))
-  time.sleep_ms(500)
+  # change red color in response to ADC value:
+  red = adc1_val_8bit
+  rgb.fill_color(get_color(red, 0, 0))
+  time.sleep_ms(50)
 
 if __name__ == '__main__':
   try:
