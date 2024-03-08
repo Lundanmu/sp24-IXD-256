2695159de31d4b9976a2e0bcc2f87b61604f94b8
Nikita Pashenkov <nikitapashenkov@gmail.com>
2024-03-08 09:03:05 -0800

    Update web_server_test.py

--- class08/web_server_test.py
+++ class08/web_server_test.py
@@ -31,6 +31,33 @@ while wifi.isconnected() == False:
 print('WiFi connection successful')
 print(wifi.ifconfig())
 
+program_state = 'ON'
+
+def web_page():
+    global program_state
+    html = """
+    <html>
+        <head>
+            <title>Web Server</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="icon" href="data:,">
+            <style>
+                html{font-family: Helvetica; display:inline-block; margin: 0px auto; text-align: center;}
+                h1{color: #0F3376; padding: 2vh;}
+                p{font-size: 1.5rem;}
+                .button{display: inline-block; background-color: #e7bd3b; border: none; border-radius: 4px; color: white; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}
+                .button2{background-color: #4286f4;}
+            </style>
+        </head>
+        <body>
+            <h1>Web Server</h1> 
+            <p>program state: <strong>""" + program_state + """</strong></p>
+            <p><a href="/?state=on"><button class="button">ON</button></a></p>
+            <p><a href="/?state=off"><button class="button button2">OFF</button></a></p>
+        </body>
+    </html>"""
+    return html
+
 def setup():
   global title0, label0
   M5.begin()
@@ -44,9 +71,37 @@ def setup():
   
   
 def loop():
-  M5.update()
-  # wait for 100 milliseconds (1/10 second):
-  time.sleep_ms(100)
+    global program_state
+    M5.update()
+    # wait for 100 milliseconds (1/10 second):
+    #time.sleep_ms(100)
+    conn, addr = s.accept()
+    print('Got a connection from %s' % str(addr))
+    request = conn.recv(1024)
+    request = str(request)
+    print('Content = %s' % request)
+    state_on = request.find('/?state=on')
+    state_off = request.find('/?state=off')
+    if state_on == 6:
+        program_state = 'ON'
+        print(program_state)
+        label0.setText(program_state)
+        #led.value(1)
+        #rgb.fill_color(0x0000ff)
+    if state_off == 6:
+        program_state = 'OFF'
+        print(program_state)
+        label0.setText(program_state)
+        #rgb_state = 'OFF'
+        #led.value(0)
+        #rgb.fill_color(0x000000)
+    #print('rgb_state =', rgb_state)
+    response = web_page()
+    conn.send('HTTP/1.1 200 OK\n')
+    conn.send('Content-Type: text/html\n')
+    conn.send('Connection: close\n\n')
+    conn.sendall(response)
+    conn.close()
   
 if __name__ == '__main__':
   try:
