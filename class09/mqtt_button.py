e1d59ed7b8cff245f4ff397ba573a689dc6d7718
Nikita Pashenkov <nikitapashenkov@gmail.com>
2024-03-22 08:08:27 -0700

    Update mqtt_button.py

--- class09/mqtt_button.py
+++ class09/mqtt_button.py
@@ -1,4 +1,4 @@
-# beginner program for M5Stack AtomS3 based on uiflow2 code structure
+# MQTT button publish and subscribe to Adafruit IO feed
 
 import os, sys, io
 import M5
@@ -15,8 +15,8 @@ ssid = 'INSERT_WIFI_NAME'
 password = 'INSERT_WIFI_PASSWORD'
 
 mqtt_client = None
-aio_user_name = 'INSERT_YOUR_AIO_USERNAME'
-aio_password = 'INSERT_YOUR_AIO_PASSWORD'
+aio_user_name = 'INSERT_ADAFRUIT_IO_USERNAME'
+aio_password = 'INSERT_ADAFRUIT_IO_PASSWORD'
 
 
 def wifi_connect():
@@ -40,7 +40,7 @@ def setup():
   global mqtt_client
   M5.begin()
   # display title ("title text", text offset, fg color, bg color, font):
-  title0 = Widgets.Title("MQTT test", 3, 0x000000, 0xffffff, Widgets.FONTS.DejaVu18)
+  title0 = Widgets.Title("MQTT button", 0, 0x000000, 0xffffff, Widgets.FONTS.DejaVu18)
   # display label ("label text", x, y, layer number, fg color, bg color, font):
   label0 = Widgets.Label("---", 3, 20, 1.0, 0xffffff, 0x000000, Widgets.FONTS.DejaVu18)
   wifi_connect()
@@ -50,23 +50,29 @@ def setup():
       port = 1883,
       user = aio_user_name,
       password = aio_password,
-      keepalive = 0
+      keepalive = 3000
   )
   mqtt_client.connect(clean_session=True)
+  mqtt_client.subscribe(aio_user_name+'/feeds/button-feed', feed_callback)
 
+
+def feed_callback(data):
+  #print('received..', data)
+  print('received..', data[1].decode())
   
 def loop():
   global mqtt_client
   M5.update()
   if BtnA.wasPressed():
     print('button pressed..')
-    mqtt_client.publish(aio_user_name+'/feeds/button-feed', '1', qos=0)
+    mqtt_client.publish(aio_user_name+'/feeds/button-feed', 'ON', qos=0)
   elif BtnA.wasReleased():
     print('button released..')
-    mqtt_client.publish(aio_user_name+'/feeds/button-feed', '0', qos=0)
-      
-  # wait for 100 milliseconds (1/10 second):
-  time.sleep_ms(100)
+    mqtt_client.publish(aio_user_name+'/feeds/button-feed', 'OFF', qos=0)
+
+  # check for messages from any subscribed feeds:
+  mqtt_client.check_msg()  
+  time.sleep_ms(100)  
   
   
 if __name__ == '__main__':
